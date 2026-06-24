81a7f442a76967be27cdde6483ef2bf90d1606fd
Aastha Jaiswal <149791911+aastha-19@users.noreply.github.com>
2026-06-24 16:30:30 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.55%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long  low=1;
+        long long  high=num;
+
+        while(low<=high){
+            long long mid=(high+low)/2;
+            long long sq=mid*mid;
+
+            if(sq==num){
+                return true;
+            }
+            if (sq<num){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
